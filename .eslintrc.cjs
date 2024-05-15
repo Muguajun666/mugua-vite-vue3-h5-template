0680a8ee3abb11c362ade880c23cebb7e5630333
muguajun666 <liushulialen@qq.com>
2024-05-15 17:13:45 +0800

    feat: add development specifications

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    './.eslintrc-auto-import.json'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    // 'prettier/prettier': 'warn',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off',
+    // 'vue/multi-word-component-names': 'off',
+    // 'no-async-promise-executor': 'off'
+    // 'no-undef': 'off'
   }
 }
